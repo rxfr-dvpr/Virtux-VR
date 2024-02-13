13632091e942734346a775b07169a8cf1351557f
Avazbek <saidaliyev.avazbek14@gmail.com>
2024-02-13 22:21:56 +0500

    header stylize finished

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,10 +3,14 @@
     <div class="container">
         <div class="row">
             <div class="header__descr">
-                <h1 class="header__descr-title" v-html="store.descr.title"></h1>
+                <h1 class="header__descr-title all-title" v-html="store.descr.title"></h1>
                 <p class="header__descr-txt">{{ store.descr.txt }}</p>
 
-                <button class="header__descr-btn all-btn">{{ store.descr.btnTxt }}</button>
+                <button class="header__descr-btn all-btn scnd">{{ store.descr.btnTxt }}</button>
+            </div>
+
+            <div class="header__img-box">
+                <img src="@/assets/images/header-img.png" alt="" class="header-img">
             </div>
         </div>
     </div>
@@ -20,7 +24,7 @@ export default {
     name: 'Header',
     data() {
         return {
-            store: headerStore()
+            store: headerStore(),
         }
     }
 }
@@ -33,13 +37,20 @@ export default {
     width: 100%;
     padding-top: 100px;
 
+    .row {
+        justify-content: space-between;
+        gap: 40px;
+    }
+
     &__descr {
+        max-width: 455px;
+        width: 100%;
         display: flex;
         flex-direction: column;
-        row-gap: 25px;
+        row-gap: 35px;
 
         &-title {
-            text-transform: capitalize;
+            text-transform: capitalize !important;
 
             .purple-clr {
                 color: var(--main-purple);
@@ -47,9 +58,23 @@ export default {
         }
 
         &-txt {
+            max-width: 355px;
+            width: 100%;
+
             &::first-letter {
                 text-transform: uppercase;
             }
+        }
+    }
+
+    &__img-box {
+        max-width: 490px;
+        width: 100%;
+        border-radius: 100px 100px 100px 250px;
+        overflow: hidden;
+
+        .header-img {
+            width: 100%;
         }
     }
 }
